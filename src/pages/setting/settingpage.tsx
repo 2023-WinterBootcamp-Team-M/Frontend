775c9f8138d8a59714d6376d1744e30a36a77fcf
ajeong <jenny2360@tukorea.ac.kr>
2024-01-08 23:31:31 +0900

    feat : 설정 메뉴 추가

--- src/pages/setting/settingpage.tsx
+++ src/pages/setting/settingpage.tsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import * as React from 'react';
 
-function settingPage() {
-  return <div>settingPage</div>;
+export default function SettingPage() {
+  return (
+    <div className="flex flex-col items-center h-screen">
+      <img src="https://i.ibb.co/kGjjkfk/Frame-427318914.png" alt="logo_icon" className="mt-10 w-28 h-auto" />
+      <div className="bg-white border-2 border-cliptab-blue mt-20 p-16 rounded-15 text-center shadow-md rounded-md ">
+        <div className="cursor-pointer mt-4">요약 설정</div>
+        <hr className="mb-4 mt-4 border-gray-300" />
+        <div className="cursor-pointer mt-4">시작 페이지 설정</div>
+        <hr className="mb-4 mt-4 border-gray-300" />
+        <div className="cursor-pointer mt-4">테마 옵션</div>
+        <hr className="mb-4 mt-4 border-gray-300" />
+        <div className="cursor-pointer mt-4 mb-4">미접속 북마크 알림 주기 설정</div>
+      </div>
+    </div>
+  );
 }
-
-export default settingPage;
