74ba7455e230c6cc02a9c04228e42432417731e9
thwlstn23 <thwlstn23@gmail.com>
2024-01-11 11:25:04 +0900

    feat : 로그인 창 구현

--- src/pages/start.tsx
+++ src/pages/start.tsx
@@ -1,31 +1,31 @@
 import * as React from 'react';
 
-export default function StartPage(){
-    return(
-    <div className='flex flex-col items-center justify-center h-screen'>
-        <img 
-        src="https://i.ibb.co/mR6vRvf/clip-tab-2-removebg-preview.png" alt="logo_icon"
-        style={{
-            paddingBottom:'15rem',
-        }}/>
-        <button
-        style={{ 
-        width: '16rem',
-        height: '3.75rem',
-        background: 'rgba(42, 114, 231, 0.95)',
-        borderRadius: '3.125rem',
-        boxShadow: '0px 2px 4px 0px rgba(0, 0, 0, 0.50)',
-        color: 'white',
-        textAlign: 'center', // 텍스트 가운데 정렬
-        textShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)', // 텍스트 쉐도우
-        fontFamily: 'Holtwood One SC', // 폰트
-        fontSize: '1rem', // 폰트 크기
-        fontStyle: 'normal', // 폰트 스타일
-        fontWeight: '400', // 폰트 굵기
-        lineHeight: '1.5rem' // 줄 높이
-        }}
-        >
-        시작하기
-        </button>
-    </div>);
+export default function StartPage() {
+  return (
+    <div className="flex flex-col items-center justify-start h-screen">
+      <img src="https://i.ibb.co/mR6vRvf/clip-tab-2-removebg-preview.png" alt="logo_icon" className="h-auto" />
+      <div className="flex items-center justify-center">
+        <div className=" mx-auto w-[90%] h-[15rem] bg-white rounded-[20px] shadow-xl border-2 border-blue-400 mb-4">
+          <form>
+            <input
+              className="w-[90%] h-11 mx-4 my-2 px-4 border-2 border-blue-400 rounded-lg text-xs shadow-xl focus:outline-blue-500"
+              type="text"
+              placeholder="아이디를 입력하세요"
+            />
+            <input
+              className="w-[90%] h-11 mx-4 my-1 px-4 border-2 border-blue-400 rounded-lg text-xs shadow-xl focus:outline-blue-500"
+              type="password"
+              placeholder="비밀번호를 입력하세요"
+            />
+          </form>
+          <div className="w-full text-gray-500 text-xs text-right px-5 py-1 mt-1">
+            <button>아이디</button>/<button>비밀번호 찾기</button> | <button>회원가입</button>
+            <button className="bg-[#0096FB] rounded-md shadow-lg text-white px-4 py-1 mx-3 mt-8 w-[90%] h-11">
+              로그인
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
