bff3973337437ff218f5fa4dc33c199590adc32c
thwlstn23 <thwlstn23@gmail.com>
2024-01-15 23:48:46 +0900

    feat : 폴더 생성 api 연동, 폴더 생성 삭제 수정 버튼 추가

--- src/components/DragNDrop.tsx
+++ src/components/DragNDrop.tsx
@@ -1,60 +1,79 @@
-import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
 import React from 'react';
-import { useRef } from "react";
-import ToolTip from "./ToolTip";
+import { useRef } from 'react';
+import ToolTip from './ToolTip';
+import axios from 'axios';
 
-const DndContainer = ({post,setPost}:any)=>{
+const DndContainer = ({ post, setPost }: any) => {
+  const popoverRef = useRef<HTMLDivElement>(null);
+  //드래그가 끝났을 때 호출되어 드래그가 끝났을때의 결과 저장
+  const handleChange = (result: any) => {
+    if (!result.destination) return;
+    const items = [...post];
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reorderedItem);
+    setPost(items);
+  };
 
-    const popoverRef = useRef<HTMLDivElement>(null);
-    //드래그가 끝났을 때 호출되어 드래그가 끝났을때의 결과 저장
-    const handleChange = (result:any) => {
-        if(!result.destination) return;
-        const items = [...post];
-        const [reorderedItem] = items.splice(result.source.index,1);
-        items.splice(result.destination.index,0,reorderedItem);
-        setPost(items);
-    }
+  const handleBookmarkDelete = async (bookmarkId: number) => {
+    await axios.delete(`http://localhost:8000/api/v1/bookmarks/{folder_id}/{bookmark_id}`);
+    console.log(`Deleting bookmark: ${bookmarkId}`);
+  };
 
-    return (
-        <DragDropContext onDragEnd={handleChange}>
-            <Droppable droppableId="cardlists">
-                {provided => (
-                    <div className="cardlits" {...provided.droppableProps} ref={provided.innerRef}>
-                        {post.map((e: any, i: number) => (
-                            <Draggable draggableId={`test-${e.id}`} index={i} key={`test-${e.id}`}>
-                                {(provided, snapshot) => {
-                                return (
-                                    <div
-                                    {...provided.draggableProps}
-                                    {...provided.dragHandleProps}
-                                    ref={provided.innerRef}
-                                    >
-                                    {/* 원하는컴포넌트 */}
-                                    <div
-                                        ref={popoverRef}
-                                        className="mx-auto w-[90%] h-fit bg-cliptab-blue rounded-md shadow-xl mb-2 px-2 py-1"
-                                        role="tooltip"
-                                    >
-                                    {
-                                    <li key={e.id} className="flex items-center">
-                                    <img className="w-4 h-4 mr-2" src={e.imageUrl} alt="Bookmark Icon" />
-                                    <ToolTip title={e.summary}>
-                                      <a href={e.url}>{e.title}</a>
-                                    </ToolTip>
-                                  </li>        
-                                    }
-                                    </div>
-                                    </div>
-                                );
-                                }}
-                            </Draggable>
-                            ))}
-                            {provided.placeholder}
+  const handleBookmarkEdit = (bookmarkId: number) => {
+    // 북마크 수정 로직
+    console.log(`Editing bookmark: ${bookmarkId}`);
+  };
+
+  return (
+    <DragDropContext onDragEnd={handleChange}>
+      <Droppable droppableId="cardlists">
+        {(provided) => (
+          <div className="cardlits" {...provided.droppableProps} ref={provided.innerRef}>
+            {post.map((e: any, i: number) => (
+              <Draggable draggableId={`test-${e.id}`} index={i} key={`test-${e.id}`}>
+                {(provided, snapshot) => {
+                  return (
+                    <div {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}>
+                      {/* 원하는 컴포넌트 */}
+                      <div
+                        ref={popoverRef}
+                        className="mx-auto w-[90%] h-fit bg-cliptab-blue rounded-md shadow-xl mb-2 px-2 py-1"
+                        role="tooltip"
+                      >
+                        {
+                          <li key={e.id} className="flex items-center">
+                            <img className="w-4 h-4 mr-2" src={e.imageUrl} alt="Bookmark Icon" />
+                            <ToolTip title={e.summary}>
+                              <a href={e.url}>{e.title}</a>
+                            </ToolTip>
+                            <button
+                              onClick={() => handleBookmarkEdit(e.id)}
+                              className="ml-auto text-blue-700 hover:text-red-700 focus:outline-none"
+                            >
+                              수정
+                            </button>
+                            <button
+                              onClick={() => handleBookmarkDelete(e.id)}
+                              className="ml-auto text-red-700 hover:text-red-700 focus:outline-none"
+                            >
+                              삭제
+                            </button>
+                          </li>
+                        }
+                      </div>
+                      {/* 원하는 컴포넌트 */}
                     </div>
-                )}
-            </Droppable>
-        </DragDropContext>
-    )
-}
+                  );
+                }}
+              </Draggable>
+            ))}
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
+    </DragDropContext>
+  );
+};
 
-export default DndContainer
+export default DndContainer;
--- src/pages/bookmark/BookmarkPage.tsx
+++ src/pages/bookmark/BookmarkPage.tsx
@@ -1,11 +1,13 @@
 import React, { useState, useRef, useEffect } from 'react';
 import ToolTip from '../../components/ToolTip';
 import DndContainer from '../../components/DragNDrop';
+import axios from 'axios';
 
 interface BookmarkFolder {
   id: number;
   title: string;
   url: string;
+  bookmarks: Bookmark[];
 }
 
 interface Bookmark {
@@ -23,37 +25,17 @@ interface BookmarkPageProps {
 const BookmarkPage: React.FC<BookmarkPageProps> = ({ title }) => {
   const [selectedFolder, setSelectedFolder] = useState<BookmarkFolder | null>(null);
   const popoverRef = useRef<HTMLDivElement>(null);
-  const [bookmarks,setBookmarks] = useState<Bookmark[]>([{
-    id: 1,
-    title: 'Bookmark 1',
-    url: 'https://www.google.com/',
-    imageUrl: 'https://i.ibb.co/X58LMLL/icon1.png',
-    summary: 'summary1',
-  },
-  {
-    id: 2,
-    title: 'Bookmark 2',
-    url: 'https://www.naver.com/',
-    imageUrl: 'https://i.ibb.co/X58LMLL/icon1.png',
-    summary: 'summary2',
-  },
-  {
-    id: 3,
-    title: 'Bookmark 3',
-    url: 'https://www.daum.net/',
-    imageUrl: 'https://i.ibb.co/X58LMLL/icon1.png',
-    summary: 'summary3',
-  },]);
-
-
-  const bookmarkFolders: BookmarkFolder[] = [
-    { id: 1, title: 'Bookmark_folder 1', url: '' },
-    { id: 2, title: 'Bookmark_folder 2', url: '' },
-    { id: 3, title: 'Bookmark_folder 3', url: '' },
-  ];
+  const [bookmarks, setBookmarks] = useState<Bookmark[]>([]);
+  const [bookmarkFolders, setBookmarkFolders] = useState<BookmarkFolder[]>([]);
+  const [folderName, setFolderName] = useState('');
+  const [isFormVisible, setIsFormVisible] = useState(false);
 
   const handleFolderClick = (folder: BookmarkFolder) => {
-    setSelectedFolder(folder);
+    if (selectedFolder && selectedFolder.id === folder.id) {
+      setSelectedFolder(null);
+    } else {
+      setSelectedFolder(folder);
+    }
   };
 
   const handlePopoverClick = (event: MouseEvent) => {
@@ -69,49 +51,119 @@ const BookmarkPage: React.FC<BookmarkPageProps> = ({ title }) => {
     };
   }, []);
 
-  const handleBookmarkDeleteClick = (bookmark: Bookmark) => {
-    console.log(`Deleting bookmark: ${bookmark.title}`);
+  // 폴더 생성
+  const handleFolderCreate = async (user_id: number, folderName: string) => {
+    try {
+      const jsonData = { name: folderName, user_id: user_id };
+      const response = await axios.post(`http://localhost:8000/api/v1/folders`, jsonData, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      console.log(response.data);
+    } catch (error) {
+      console.error('Error creating folder:', error);
+    }
+  };
+
+  const updateSelectedFolderBookmarks = (newBookmarks: Bookmark[]) => {
+    if (selectedFolder) {
+      const updatedFolder = { ...selectedFolder, bookmarks: newBookmarks };
+      setSelectedFolder(updatedFolder);
+    }
+  };
+
+  const handleFolderCreateClick = () => {
+    setIsFormVisible(true);
+  };
+
+  const handleFolderCreateSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+    const user_id = 1;
+    await handleFolderCreate(user_id, folderName);
+    setFolderName('');
+    setIsFormVisible(false);
   };
 
   return (
     <div className="flex flex-col items-center">
       <img className="mt-10 w-28 h-auto mb-2" src="https://i.ibb.co/kGjjkfk/Frame-427318914.png" alt={title} />
-      <div className="text-gray-500 self-start text-xl">
+      <div className="text-gray-500 self-start text-xl flex items-center">
         <h2 className="ml-4">북마크</h2>
+        <button
+          onClick={handleFolderCreateClick}
+          className="bg-blue-600 text-white rounded px-2 py-0 hover:bg-blue-800 ml-2 text-sm"
+        >
+          생성
+        </button>
       </div>
-      <div className="mx-auto mt-4 w-[90%] h-[30rem] bg-white rounded-[20px] shadow-xl border-2 border-blue-600 mb-4">
+      {isFormVisible && (
+        <form
+          onSubmit={handleFolderCreateSubmit}
+          className="mx-auto w-[70%] h-[rem] bg-white rounded-[20px] shadow-xl border-2 border-blue-400 p-4 mb-4"
+        >
+          <label className="text-sm">
+            폴더 이름:
+            <input
+              type="text"
+              value={folderName}
+              onChange={(e) => setFolderName(e.target.value)}
+              className="ml-2 border-2 border-blue-400 rounded px-2 py-1"
+            />
+          </label>
+          <button type="submit" className="bg-blue-600 text-white rounded px-2 py-0 hover:bg-blue-800 ml-2 text-sm">
+            생성
+          </button>
+          <button type="reset" className="bg-blue-600 text-white rounded px-2 py-0 hover:bg-blue-800 ml-2 text-sm">
+            취소
+          </button>
+        </form>
+      )}
+      <div
+        className={`mx-auto mt-4 w-[90%] bg-white rounded-[20px] shadow-xl border-2 border-blue-400 mb-4 ${
+          selectedFolder ? 'h-[30rem]' : 'h-[12rem]'
+        }`}
+      >
         <ul className="text-sm p-5 leading-10">
           {bookmarkFolders.map((folder) => (
             <li key={folder.id} className="flex items-center mb-2">
-              <img className="w-4 h-4 mr-2" src="https://i.ibb.co/zFxjK9M/icon.png" alt="Folder Icon" />
+              <img className="w-4 h-4 mr-2" src="https://i.ibb.co/nsvNYV1/folder.png" alt="Folder Icon" />
               <a href="#" onClick={() => handleFolderClick(folder)}>
                 {folder.title}
               </a>
+              <button
+                onClick={() => handleFolderEdit(folder.id)}
+                className="ml-5 text-blue-700 hover:text-red-700 focus:outline-none"
+              >
+                수정
+              </button>
+              <button
+                onClick={() => handleFolderDelete(folder.id)}
+                className="ml-5 text-red-700 hover:text-red-700 focus:outline-none"
+              >
+                삭제
+              </button>
             </li>
           ))}
         </ul>
         {selectedFolder && (
-          <div className=' w-[90%] h-[17rem] bg-[#DFEBFF] rounded-[20px] shadow-xl mb-4 mx-auto mt-[-1rem] py-4'>
-          <DndContainer post={bookmarks} setPost={setBookmarks}/>
+          <div className=" w-[90%] h-[17rem] bg-[#DFEBFF] rounded-[20px] shadow-xl mb-4 mx-auto mt-[-1rem] py-4">
+            <DndContainer post={selectedFolder.bookmarks} setPost={updateSelectedFolderBookmarks}>
+              <div>삭제</div>
+            </DndContainer>
           </div>
         )}
       </div>
       <div className="text-gray-500 self-start text-xl flex items-center">
         <h2 className="ml-4">북마크 편집</h2>
-        <img className="ml-2 w-4 h-4" src="https://i.ibb.co/Bj1DmqY/add-square.png" alt="Add Icon" />
+        <img className="ml-2  w-4 h-4" src="https://i.ibb.co/Bj1DmqY/add-square.png" alt="Add Icon" />
       </div>
-      <div className="mx-auto mt-4 w-[90%] h-[9rem] bg-white rounded-[20px] shadow-xl border-2 border-blue-600 mb-4">
+      <div className="mx-auto mt-4 w-[90%] h-[9rem] bg-white rounded-[20px] shadow-xl border-2 border-blue-400 mb-4">
         <ul className="text-sm p-5 leading-8 m-0">
           {bookmarks.map((bookmark) => (
             <li key={bookmark.id} className="flex items-center">
               <img className="w-4 h-4 mr-2" src={bookmark.imageUrl} alt="Bookmark Icon" />
               <a href={bookmark.url}>{bookmark.title}</a>
-              <button
-                onClick={() => handleBookmarkDeleteClick(bookmark)}
-                className="ml-auto text-red-500 hover:text-red-700 focus:outline-none"
-              >
-                Del
-              </button>
             </li>
           ))}
         </ul>
