4eed574712ed0be5e1554ef373489fdcf324ad12
dongmin <dongmin11566@gmail.com>
2024-02-01 02:39:56 +0900

    style : 알림 컴포넌트 반응형 적용

--- src/pages/alarm/notificationItem.tsx
+++ src/pages/alarm/notificationItem.tsx
@@ -13,7 +13,7 @@ const NotificationItem = ({ notification}) => {
   };
 
   return (
-    <div className={`relative w-full h-max mb-4 p-4 rounded-[20px] shadow-xl  flex flex-col justify-between ${opt_theme ? "bg-dark-component" : "border-2 border-cliptab-blue bg-white"}`}>
+    <div className={`relative w-full h-[15%] mb-4 rounded-[20px] shadow-xl  flex flex-col justify-evenly ${opt_theme ? "bg-dark-component" : "border-2 border-cliptab-blue bg-white"}`}>
       <button
         className={`absolute -top-2 -right-2 ${opt_theme ? "desaturate" : ""}`}
         onMouseEnter={() => setButtonImage('https://i.ibb.co/rss1tFV/Group-1000002293.png')}
@@ -23,16 +23,16 @@ const NotificationItem = ({ notification}) => {
         <img src={buttonImage} alt="close_btn" className="w-7 h-7" />
       </button>
 
-      <div className="flex items-center">
-        <img src='https://i.ibb.co/Jp8Xkwr/5.jpg' alt={notification.name} className="w-10 h-10 rounded-xl ml-4 mr-4"/>
-        <div>
+      <div className="flex justi items-center mx-auto w-[90%]">
+        <img src='https://i.ibb.co/Jp8Xkwr/5.jpg' alt={notification.name} className="w-12 h-12 rounded-xl mx-auto"/>
+        <div className='w-[70%] items-center'>
           <a
             href={notification.bookmark_url}
             target="_blank"
             rel="noreferrer"
             className="text-gray-600 text-sm leading-none underline"
           >
-            {notification.bookmark_url.length > 30 ? `${notification.bookmark_url.slice(0, 30)}...` : notification.bookmark_url}
+            {notification.bookmark_url.length > 20 ? `${notification.bookmark_url.slice(0, 20)}...` : notification.bookmark_url}
           </a>
           <div className={`text-xs mt-1 ${opt_theme ? "text-dark-text" : "text-gray-950"}`}>
             <div>미접속 {notification.accumulated_days}일이 경과했습니다.</div>
@@ -41,13 +41,13 @@ const NotificationItem = ({ notification}) => {
         </div>
       </div>
 
-      <div className="flex justify-between mt-3">
-        <button onClick={handleRedirect} className={`ml-8 w-24 h-7 rounded-md ${opt_theme ? "bg-cliptab-blue desaturate" : "bg-cliptab-blue text-white"}`}>
+      <div className="flex justify-evenly items-center w-[90%] h-[20%] mx-auto">
+        <button onClick={handleRedirect} className={` w-[43%] h-full text-center rounded-md ${opt_theme ? "bg-cliptab-blue desaturate" : "bg-cliptab-blue text-white"}`}>
           접속
         </button>
         <button 
         onClick={()=>deleteAlarm(userId,notification.id,setAlarmList)}
-        className={`mr-8 w-24 h-7 rounded-md ${opt_theme ? "bg-dark-btn text-dark-text" : " bg-gray-400 text-white"}`}>삭제</button>
+        className={`w-[43%] h-full text-center rounded-md ${opt_theme ? "bg-dark-btn text-dark-text" : " bg-gray-400 text-white"}`}>삭제</button>
       </div>
     </div>
   );
